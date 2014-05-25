35d570c3271a5e6df9ac7d97f86c91c56d4764ff
MacrossFiru <macrossfiru@gmail.com>
2014-05-25 13:13:29 +0000

    Following more of the Laravel Blog tutorial, added model and routes, included tutorial link file

---
+++ app/models/user.php
@@ -0,0 +1,11 @@
+<?php
+
+class User extends Eloquent {
+  
+  public function posts()
+  {
+    return $this->has_many('Post');
+  }
+  
+  
+}
--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,68 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	//return View::make('hello');
+  $posts = Post:with('user')->order_by('updated_at', 'desc')->paginate('5');
+  return View::make('home')->with('posts', $posts);
+});
+
+
+//When a user has logged in we visit they are taken to creating new post
+Route::get('admin', array('before'=>'auth','do'=>function() {
+  $user = Auth::user();
+  return View::make('new')->with('user', $user);
+}));
+
+
+Route::delete('post/(:num)', array('before'=>'auth', 'do'=>function($id){
+  $delete_post = Post::with('user')->find($id);
+  $delete_post->delete();
+  return Redirect::to('/')
+    ->with('success_message', true);
+}));
+
+
+//When a new post is submitted it is handled here
+Route::post('admin', array('before'=>'auth','do'=>function(){
+  $new_post = array(
+    'post_title'=>Input::get('post_title'),
+    'post_body'=>Input::get('post_body'),
+    'post_author'=>Input::get('post_author')
+  );  
+  
+  $validation = Validator::make($new_post, $rules);
+  if($validation->fails()) {
+    return Redirect::to('admin')
+      ->with('user', Auth::user())
+      ->with_errors($validation)
+      ->with_input();
+  }
+  //create new post after passing validation
+  $post = new Post($new_post);
+  $post->save();
+  //redirect to view all posts
+  return Redirect::to('/');
+}));
+
+//Present the login form
+Route::get('login', function(){
+  return View::make('login');
+});
+
+//Process the login request
+Route::post('login', function(){
+  $userinfo = array('username'=>Input:get('username'),
+                   'password'=>Input:get('password')
+                   );
+  if(Auth::attempt($userinfo)) {
+    return Redirect::to('admin');
+  } else {
+    return Redirect::to('login')->with('login_errors', true);
+  }
+});
+
+//Process logout request
+Route::get('logout',function(){
+  Auth::logout();
+  return Redirect::to('/');
 });
